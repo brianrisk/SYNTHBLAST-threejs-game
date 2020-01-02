ab2514387579d8ddb646d0e65be7d74c05afb6a4
Brian Risk <brian@geneffects.com>
2020-01-01 21:16:41 -0500

    minor

---
+++ js/classes/UI.js
@@ -0,0 +1,13 @@
+
+
+class UI {
+
+    constructor() {
+        let isMobile = 'ontouchstart' in document.documentElement;
+        let instructionText;
+        let b1, b2, b3, b4, b6, b7, b8, b9, bFire;
+
+    }
+}
+
+export default UI;
